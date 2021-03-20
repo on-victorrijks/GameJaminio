7fc14b48d07732ee8f2d55905195c71fd74f83fe
Victor Rijks <victor.rij@hotmail.com>
2021-03-20 16:06:34 +0100

    hugo test wola

--- core/painter.py
+++ core/painter.py
@@ -5,27 +5,32 @@ def drawMap(pg,mapData,partData,blockSize):
 
     sprites = []
 
-    for line in mapData:
+    for lineIndex,line in enumerate(mapData):
         for index in range(partData[0],partData[1]+1):
             block = line[index]
 
+            pos = [
+                index - partData[0],
+                lineIndex
+            ]
+
             if block == '0':
-                imagePath = "BLUE"
-                sprites.append(Block(pg, blockSize, imagePath))
+                imagePath = "BLUE" 
+                sprites.append(Block(pg, blockSize, imagePath,pos))
             elif block == '1':
                 imagePath = "WHITE"
-                sprites.append(Block(pg, blockSize, imagePath))
+                sprites.append(Block(pg, blockSize, imagePath,pos))
     
     return sprites
 
 class Block(pg.sprite.Sprite):
 
-    def __init__(self,pg,blockSize,imagePath):
+    def __init__(self,pg,blockSize,imagePath,pos):
 
         # TO ADD IMAGE SPRITES
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((blockSize, blockSize))
         self.image.fill(imagePath)
         self.rect = self.image.get_rect()
-        self.rect.center = (blockSize / 2, blockSize / 2)
+        self.rect.center = (pos[0],pos[1])
 
